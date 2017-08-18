53822926c3bf4beab2238217c556cc347cd29e23
sarahh2000 <sarah.huang2000@gmail.com>
2017-08-18 09:42:35 -0700

    Hello World Demonstration

---
+++ Hello_world/src/Hello_world.java
@@ -0,0 +1,13 @@
+/**
+ * Hello_world.
+ * 
+ * Sarah Huang 
+ * Aug2017
+ */
+public class Hello_world
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World! From Sarah"); //Display the string
+    }
+}
