d0ec6058b4dd864dc1934d77376d7887e9bc7b34
sarahh2000 <sarah.huang2000@gmail.com>
2017-08-23 10:51:57 -0700

    Project_2

---
+++ Project_2/src/Project_2.java
@@ -0,0 +1,12 @@
+//Sarah Huang
+//23Aug2017
+//Project_2, use print and println
+public class Project_2 {
+
+	public static void main(String[] args) {
+		System.out.print("Hi there!");
+		System.out.print("hello");
+	
+	}
+
+}
