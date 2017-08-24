9f0cf16126f2a699cda346ac1d7d8d5b417a3c27
sarahh2000 <sarah.huang2000@gmail.com>
2017-08-24 09:36:23 -0700

    hello

--- Project2B/src/Project2B.java
+++ Project2B/src/Project2B.java
@@ -3,7 +3,7 @@
 //Project2B using println and print
 public class Project2B {
 	public static void main(String[] args) {
-		System.out.print("HELLO! ");
+		System.out.print("HELLO!! ");
 		System.out.println("this is stupid");
 		System.out.println("ok bye");
 	}
