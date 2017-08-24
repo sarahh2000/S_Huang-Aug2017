acf809f4cf60682a559f7644a897257b3c9e9a8c
sarahh2000 <sarah.huang2000@gmail.com>
2017-08-24 09:30:34 -0700

    project 2b

---
+++ Project2B/src/Project2B.java
@@ -0,0 +1,11 @@
+//Sarah Huang
+//24Aug2017
+//Project2B using println and print
+public class Project2B {
+	public static void main(String[] args) {
+		System.out.print("HELLO! ");
+		System.out.println("this is stupid");
+		System.out.println("ok bye");
+	}
+
+}
