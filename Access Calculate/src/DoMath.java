9269231d3f4c1054b6da6d939c9c9c99ef7217f5
sarahh2000 <sarah.huang2000@gmail.com>
2017-09-07 09:50:39 -0700

    completed isdivisbleby

--- Access Calculate/src/DoMath.java
+++ Access Calculate/src/DoMath.java
@@ -5,7 +5,7 @@
 public class DoMath {
 
 	public static void main(String[] args) {
-		System.out.println(Calculate.toRadians(360));
+		System.out.println(Calculate.foil(2,3,6,-7,"n"));
 
 	}
 
--- Calculate Library/src/Calculate.java
+++ Calculate Library/src/Calculate.java
@@ -20,7 +20,7 @@ public class Calculate {
 	}
 	
 	//averages three values
-	public static double average(double operand1, double operand2, double operand3) {
+	public static double average(double operand1,double operand2,double operand3) {
 		return (operand1+operand2+operand3)/3;
 	}
 	
@@ -35,17 +35,36 @@ public class Calculate {
 	}
 	
 	//provides the coefficients of a quadratic value in standard form and returns the discriminant
-	public static double discriminant(double a, double b, double c) {
+	public static double discriminant(double a,double b,double c) {
 		return((b*b)+(-4)*a*c);
 	}
 	
 	//converts a mixed number into an improper fraction
-	public static double toImproperFrac(int wholeNumber,int numerator,int denominator) {
-		double newNumerator=(wholenumber*denominator)+numerator;
-		//fix string double
-		return String(newNumerator/denominator);
+	public static String toImproperFrac(int wholeNumber,int numerator,int denominator) {
+		int newNumerator=(wholeNumber*denominator)+numerator;
+				return(newNumerator+"/"+denominator);
 	}
 	
+	//converts improper fraction to a mixed number
+	public static String toMixedNum(int numerator,int denominator) {
+		int wholeNumber=numerator/denominator;
+		int newNumerator=numerator-wholeNumber*denominator;
+		return(wholeNumber+"_"+newNumerator+"/"+denominator);
+	}
+	
+	//converts a binomial multiplication into a quadratic formula
+	public static String foil(int a,int b,int c,int d,String n) {
+		return(a*c+"n^2"+"+"+(a*d+b*c)+"n"+"+"+b*d);
+	}
+	
+	//determines whether or not one integer is evenly divisible by another
+	public static boolean isDivisibleBy(int divisor,int dividend) {
+		if (divisor%dividend==0) {
+			return true;
+		}else {
+			return false;
+		}
+	}
 }
 
 
