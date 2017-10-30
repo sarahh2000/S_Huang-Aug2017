b6b3ac536d297a61fba995e2c319f0b41a45a90f
sarahh2000 <sarah.huang2000@gmail.com>
2017-10-29 17:23:45 -0700

    complete

--- Split String/src/Split.java
+++ Split String/src/Split.java
@@ -10,12 +10,27 @@ public class Split {
 		System.out.println(Arrays.toString("I reallyreallyreally likeapples".split("really")));
 		System.out.println(Arrays.toString("reallyI reallyreally likeapples".split("really")));
 		System.out.println(Arrays.toString("I reallyreally likeapples really".split("really")));
-		System.out.println(getFilling("applespineapplesbreadlettustomatobaconmayohambreadcheese"));
+		System.out.println(getFilling("breadapplespineapplesbreadlettucetomatobreadbaconmayohambreadcheese"));
+		System.out.println(spaceGetFilling("bread apples pineapples bread lettuce tomato bread bacon mayo ham bread cheese"));
 	}
 	
+	//part one
 	public static String getFilling (String filling) {
+		String insides="";
 		String[] array=filling.split("bread");
-		return array[1];
-		
+		for(int i=1;i<array.length-1;i++) {
+			insides+=array[i];
+		}
+		return insides;
+	}
+	
+	//part two
+	public static String spaceGetFilling (String filling) {
+		String everything="";
+		String []array=filling.split(" ");
+		for(int i=0;i<array.length;i++) {
+			everything+=array[i];
+		}
+		return getFilling(everything);
 	}
 }
