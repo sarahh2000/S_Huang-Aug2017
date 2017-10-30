a6a403f03b29d2527169fb231b850ebbaab33f68
sarahh2000 <sarah.huang2000@gmail.com>
2017-10-30 09:44:10 -0700

    complete

--- Split String/src/Split.java
+++ Split String/src/Split.java
@@ -13,7 +13,7 @@ public class Split {
 		System.out.println(Arrays.toString("breadcheesebreadhammayobread".split("bread")));
 		System.out.println(getFilling("breadapplespineapplesbreadlettucetomatobreadbaconmayohambreadcheese"));
 		System.out.println(spaceGetFilling("bread apples pineapples bread lettuce tomato bread bacon mayo ham bread cheese"));
-		System.out.println(getFilling("breadcheesebreadhammayobread"));
+		System.out.println(getFilling("cheesebreadhammayobread"));
 	}
 	
 	//part one
