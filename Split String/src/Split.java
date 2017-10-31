f9b69a2bf3c77a55bbe72a63d74d96d1787e4b09
sarahh2000 <sarah.huang2000@gmail.com>
2017-10-31 01:21:27 -0700

    fixed

--- Split String/src/Split.java
+++ Split String/src/Split.java
@@ -9,21 +9,27 @@ public class Split {
 		System.out.println(Arrays.toString("I reallyreally likeapples".split("really")));
 		System.out.println(Arrays.toString("I reallyreallyreally likeapples".split("really")));
 		System.out.println(Arrays.toString("reallyI reallyreally likeapples".split("really")));
-		System.out.println(Arrays.toString("I reallyreally likeapples really".split("really")));
-		System.out.println(Arrays.toString("breadbreadbreadbread".split("bread")));
+		System.out.println(Arrays.toString("I reallyreally likeapples really likeapples".split("really")));
+		System.out.println(Arrays.toString("breadcheesehammayobreadcheesebread".split("bread")));
 		System.out.println(getFilling("breadapplespineapplesbreadlettucetomatobreadbaconmayohambreadcheese"));
 		System.out.println(spaceGetFilling("bread apples pineapples bread lettuce tomato bread bacon mayo ham bread cheese"));
-		System.out.println(getFilling("cheesebreadhammayobread"));
+		System.out.println(getFilling("breadcheesehammayocheesebreadcheese"));
 	}
 	
 	//part one
 	public static String getFilling (String everything) {
+		String insides="";
+		String[] array=everything.split("bread");
+		String[] array2=everything.split("b");
 		if(everything.indexOf("bread")<0||everything.equals("bread")) {
 			return "not a sandwich";
-		}else {
-			String insides="";
-			String[] array=everything.split("bread");
+		}else if(array2[array2.length-1].equals("read")) {
 			for(int i=1;i<array.length;i++) {
+				insides+=array[i];
+			}
+			return insides;
+		}else {
+			for(int i=1;i<array.length-1;i++) {
 				insides+=array[i];
 			}
 			if(insides.equals("")) {
