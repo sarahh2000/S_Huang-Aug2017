3fefe3a803b2de43cee982314b149c4c5411eb59
sarahh2000 <sarah.huang2000@gmail.com>
2017-11-02 09:51:48 -0700

    p5

--- ArraysLab3/src/ArraysLab3.java
+++ ArraysLab3/src/ArraysLab3.java
@@ -35,4 +35,15 @@ public class ArraysLab3 {
 		}
 		return sum;
 	}
+	public static void rotateRight(int[] arr) {
+		int temp=arr[arr.length-1];
+		for(int i=arr.length-2;i>=0;i--) {
+			arr[i+1]=arr[i];
+		}
+		arr[0]=temp;
+	}
+	public static void main (String[] args) {
+		int [] arr1= {5, 10, 15, 20, 25, 30, 35, 40};
+		int [] arr2= {7,14, 21, 28, 35, 42, 49, 56};
+	}
 }
