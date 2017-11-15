6564128705581f9fc1426247c1042b4d57a3971a
sarahh2000 <sarah.huang2000@gmail.com>
2017-11-15 11:31:41 -0800

    part2

--- FracCalc/src/fracCalc/FracCalc.java
+++ FracCalc/src/fracCalc/FracCalc.java
@@ -10,21 +10,45 @@ public class FracCalc {
 
     public static void main(String[] args) {
     	Scanner userInput= new Scanner(System.in);
-    	System.out.println("What is your input?");
-    	String input=userInput.nextLine();
-    	System.out.println(produceAnswer(input));
-    	
+    	boolean done=true;
+    	while(done) {
+    		System.out.println("What is your input?");
+        	String input=userInput.nextLine();
+        	if(input.equals("quit")){
+        		done=false;
+        	}
+        	else{
+        		System.out.println(produceAnswer(input));
+        	}
+        	
+    	}
     }
-    
     public static String produceAnswer(String input){
     	String [] inputArray=input.split(" ");
     	String firstOperand=inputArray[0];
     	String operator=inputArray[1];
     	String secondOperand=inputArray[2];
-   
-        return secondOperand;
+    	parseValues(firstOperand);
+    	return parseValues(secondOperand);
     }
-
+    public static String parseValues(String input) {
+    	String [] underscoreSplit=input.split("_");
+    	String [] slashSplit=input.split("/");
+    	String whole="0";
+    	String numerator="0";
+    	String denominator="1";
+    	if(input.contains("_")==true&&input.contains("/")==true){
+    		whole=underscoreSplit[0];
+    		numerator=underscoreSplit[1].split("/")[0];
+    		denominator=slashSplit[1];
+    	}else if(input.contains("_")==false&&input.contains("/")==true) {
+    		numerator=slashSplit[0];
+    		denominator=slashSplit[1];
+    	}else{
+    		whole=input;
+    	}
+    	return "whole:"+whole+" numerator:"+numerator+" denominator:"+denominator;
+    }
     // TODO: Fill in the space below with any helper methods that you think you will need
     
 }
