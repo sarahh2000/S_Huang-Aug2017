76e16cd8aee09e41dc77d3a5565a112c8225d72c
sarahh2000 <sarah.huang2000@gmail.com>
2017-11-16 09:52:24 -0800

    doing add/subtract

--- FracCalc/src/fracCalc/FracCalc.java
+++ FracCalc/src/fracCalc/FracCalc.java
@@ -31,7 +31,7 @@ public class FracCalc {
     	parseValues(firstOperand);
     	return parseValues(secondOperand);
     }
-    public static String parseValues(String input) {
+    public static int[] parseValues(String input) {
     	String [] underscoreSplit=input.split("_");
     	String [] slashSplit=input.split("/");
     	String whole="0";
@@ -47,7 +47,42 @@ public class FracCalc {
     	}else{
     		whole=input;
     	}
-    	return "whole:"+whole+" numerator:"+numerator+" denominator:"+denominator;
+    	int[]parsedInputArray=new int[3];
+    	parsedInputArray[0]=Integer.parseInt(whole);
+    	parsedInputArray[1]=Integer.parseInt(numerator);
+    	parsedInputArray[2]=Integer.parseInt(denominator);
+    	return parsedInputArray;
+    }
+    public static String addSubtract(int[] operand1, int[] operand2, String operator) {
+    	commonDenominator(improperFrac(operand1), improperFrac(operand2));
+    	//finish
+    	
+    }
+    public static int[] improperFrac(int[]fraction) {
+    	int[]improper=new int[2];
+    	if(fraction[0]!=0) {
+    		improper[1]=fraction[2];
+    		if(fraction[0]<0) {
+    			improper[0]=(-1)*fraction[0]*fraction[2]+fraction[1];
+    		}else {
+    			improper[0]=fraction[0]*fraction[2]+fraction[1];
+    		}
+    	}else if(fraction[0]==0&&fraction[1]!=0) {
+    		improper[0]=fraction[1];
+    		improper[1]=fraction[2];
+    	}else {
+    		improper[0]=0;
+    		improper[1]=1;
+    	}
+    	return improper;
+    }
+    public static void commonDenominator(int[]operand1, int[] operand2) {
+    	int denominator1=operand1[1];
+    	int denominator2=operand2[2];
+    	operand1[0]*=denominator2;
+    	operand1[1]*=denominator2;
+    	operand2[0]*=denominator1;
+    	operand2[1]*=denominator1;
     }
     // TODO: Fill in the space below with any helper methods that you think you will need
     
