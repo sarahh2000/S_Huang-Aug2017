de4e280f6a966c197768f9b888714cf269e9eca3
sarahh2000 <sarah.huang2000@gmail.com>
2018-03-23 09:50:55 -0700

    starting cp5

--- TextExcel/src/textExcel/FormulaCell.java
+++ TextExcel/src/textExcel/FormulaCell.java
@@ -14,34 +14,47 @@ public class FormulaCell extends RealCell{
 		String [] inputArray=text.split(" ");
 		String result="";
 		//for loop that does math for the first three spaces, and then sets the result equal to the first space, then adds/subtracts/multiplies/divides the result to the next number
-		if(inputArray.length==3) {
+		if(inputArray.length==3&&!(inputArray[1].equals("sum"))&&!(inputArray[1].equals("avg"))) {
 			return Double.parseDouble(inputArray[1]);
 		}else {
-			for (int i=3; i<inputArray.length-1;i+=2) {
+			if(inputArray[1].equals("sum")) {
+				return 0;
+			}else if(inputArray[1].equals("avg")) {
+				return 0;
+			}else {
 
-				String firstOperand=inputArray[1];
-				String operator=inputArray[i-1];
-				String secondOperand=inputArray[i];
-
-				if(operator.equals("+")||operator.equals("-")) {
-					if(firstOperand.equals("0")) {
-						if(operator.equals("+")) {
-							result=secondOperand;
-						}else {
-							result="-"+secondOperand;
-						}
-					}else if(secondOperand.equals("0")) {
-						result=firstOperand;
-					}else {
-						result=addSubtract(firstOperand, secondOperand, operator)+"";
+				for (int i=3; i<inputArray.length-1;i+=2) {
+					String firstOperand=inputArray[1];
+					String operator=inputArray[i-1];
+					String secondOperand=inputArray[i];
+					
+					String character=inputArray[1].substring(0, 1).toUpperCase();
+					if(character.charAt(0)>=65&&character.charAt(0)<=76) {
+						//get cell
 					}
+					
+					
 
-				}else {
-					result=multiplyDivide(firstOperand, secondOperand, operator)+"";
+					if(operator.equals("+")||operator.equals("-")) {
+						if(firstOperand.equals("0")) {
+							if(operator.equals("+")) {
+								result=secondOperand;
+							}else {
+								result="-"+secondOperand;
+							}
+						}else if(secondOperand.equals("0")) {
+							result=firstOperand;
+						}else {
+							result=addSubtract(firstOperand, secondOperand, operator)+"";
+						}
+
+					}else {
+						result=multiplyDivide(firstOperand, secondOperand, operator)+"";
+					}
+					inputArray[1]=result;
 				}
-				inputArray[1]=result;
+				return Double.parseDouble(result);
 			}
-			return Double.parseDouble(result);
 		}
 
 	}
